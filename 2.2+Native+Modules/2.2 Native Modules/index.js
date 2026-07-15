f123fd2cedbf301b19ddfa876ee8b322e3c4d190
Mohd-Suhail-Gazi <suhailgazi46@gmail.com>
2026-07-16 00:32:08 +0530

    Add files via upload

---
+++ 2.2+Native+Modules/2.2 Native Modules/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+fs.writeFile("message.txt", "hello from node js", err => {
+    if (err) throw err;
+    console.log("file has been saved");
+
+})
+
+fs.readFile("message.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+}
+)
